5e0a8f0c680337d449bd5dfcb50a8b799dac0d6c
ydproyectosrz398-oss <ydproyectosrz398@gmail.com>
2026-05-13 15:37:29 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
+const CACHE_NAME = "radio-cache-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./cover.png",
+  "./icon.png"
+];
+
+// Instalar el Service Worker y guardar los archivos en la memoria
 self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder con los archivos guardados si no hay internet
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
